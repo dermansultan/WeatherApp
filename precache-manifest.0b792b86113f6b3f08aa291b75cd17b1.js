6b0e0cf283f49b55346115d5a0eebf0346f7c721
dermansultan <abdurrehmansultan4@gmail.com>
2020-09-01 13:24:43 -0400

    Updates

---
+++ precache-manifest.0b792b86113f6b3f08aa291b75cd17b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dffd45f4aaca34426345b6c89958e35",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "2f004b988e7720f7b15a",
+    "url": "/WeatherApp/static/css/main.480340ea.chunk.css"
+  },
+  {
+    "revision": "769be6c037fa0b8c4756",
+    "url": "/WeatherApp/static/js/2.0bc345ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.0bc345ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f004b988e7720f7b15a",
+    "url": "/WeatherApp/static/js/main.639e4464.chunk.js"
+  },
+  {
+    "revision": "5fdeb778b69e8ff58bbb",
+    "url": "/WeatherApp/static/js/runtime-main.8c7ddbed.js"
+  }
+]);
